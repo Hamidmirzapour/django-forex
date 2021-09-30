75dffb8ff12862acb2b1e4de75afb796f6c8e6ff
Hamid Mirzapour <h.r.mirzapour@gmail.com>
2021-09-30 22:38:06 +0330

    CRUD Operation

--- stocks/quotes/models.py
+++ stocks/quotes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.db.models import fields
 
-# Create your models here.
+
+class Portfolio(models.Model):
+    base_currency = fields.CharField(max_length=10)
+    quote_currency = fields.CharField(max_length=10)
+
+    def __str__(self) -> str:
+        return self.base_currency + '/' + self.quote_currency
